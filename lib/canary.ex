48cd13d2ff992e45354939c36215c5892dd63b85
Chris Kelly <cpjkelly@gmail.com>
2015-04-18 12:28:08 -0700

    Use function- rather than module-style plugs in Canary

--- lib/canary.ex
+++ lib/canary.ex
@@ -1,20 +1,11 @@
-defmodule Canary.LoadResource do
-
-  def init(options) do
-    options
-  end
-
-  def call(conn, opts) do
-    conn
-    |> load_resource(opts[:resource])
-  end
+defmodule Canary do
 
   @doc """
   Load the resource given by conn.params["id"] and
   add it to conn.assigns.
   """
-  def load_resource(conn, resource) do
-    fetched_resource = resource
+  def load_resource(conn, opts) do
+    fetched_resource = opts[:resource]
     |> fetch_resource(conn.params["id"])
 
     %{ conn | assigns: Map.put(conn.assigns, :fetched_resource, fetched_resource) }
@@ -24,7 +15,7 @@ defmodule Canary.LoadResource do
   Fetch the resource from the database. Initially only supports ecto.
   Need a place to define which repo to use
   """
-  def fetch_resource(resource, resource_id) do
+  defp fetch_resource(resource, resource_id) do
     Cooking.Repo.get(resource, resource_id)
   end
 end
