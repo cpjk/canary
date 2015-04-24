86c142b7532d6ab9469a0f4d5c69f2d09902f951
Chris Kelly <cpjkelly@gmail.com>
2015-04-23 20:35:49 -0700

    Release 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Canary.Mixfile do
 
   def project do
     [app: :canary,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.0",
      package: package,
      description: """
