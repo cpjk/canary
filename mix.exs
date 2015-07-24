e01572b707808e596402a49b6bf6116f651ce2ef
Fabian Keunecke <fkeunecke@frobese.de>
2015-07-24 10:32:24 +0200

    Release v0.9.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Canary.Mixfile do
 
   def project do
     [app: :canary,
-     version: "0.9.1",
+     version: "0.9.2",
      elixir: "~> 1.0",
      package: package,
      description: """
