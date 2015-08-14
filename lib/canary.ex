c7b045fb5ad6a85401a51d2bb7ceaa251e8b14d3
Chris Kelly <cpjkelly@gmail.com>
2015-08-14 14:23:40 -0700

    Add moduledoc to Canary

--- lib/canary.ex
+++ lib/canary.ex
@@ -1,2 +1,7 @@
 defmodule Canary do
+  @moduledoc """
+  Canary provides plugs to use for authorization. The plug functions are defined in `Canary.Plugs`
+
+  In order to use the plugs, just `import Canary.Plugs`
+  """
 end
