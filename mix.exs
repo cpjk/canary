5e623cf5981ffd89e4f12704da59bfbbe6f2b7e1
Chris Kelly <cpjkelly@gmail.com>
2016-06-02 19:54:35 -0400

    Bump plug to 1.0

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule Canary.Mixfile do
     [
      {:ecto, "~> 1.1"},
      {:canada, "~> 1.0.0"},
-     {:plug, ">= 0.11.3"},
+     {:plug, "~> 1.0"},
      {:ex_doc, "~> 0.7", only: :dev},
      {:earmark, ">= 0.0.0", only: :dev},
      {:mock, ">= 0.0.0", only: :test}
